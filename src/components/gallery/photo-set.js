5b91e504e8d510b1372a929d1edd8e17656c2e63
Mitchel Pigsley <mpigsley@gmail.com>
2018-04-14 09:29:44 -0500

    Add a bunch of new photos

--- src/components/gallery/photo-set.js
+++ src/components/gallery/photo-set.js
@@ -1,45 +1,81 @@
+import BabyBoy from 'images/baby_boy.jpeg';
 import BabyShower from 'images/baby_shower.jpg';
+import Baby from 'images/baby.jpeg';
 import BacheloretteParty from 'images/bachelorette_party.jpg';
 import BaconAndEggs from 'images/bacon_and_eggs.jpg';
 import Baseball from 'images/baseball.jpg';
+import BeachTime from 'images/beach_time.jpeg';
 import BirdsAndFlowers from 'images/birds_and_flowers.jpg';
 import BirdsAndNests from 'images/birds_and_nests.jpg';
 import Birthday from 'images/birthday.jpg';
+import Bridesmaids from 'images/bridesmaids.jpeg';
+import BrownBears from 'images/brown_bears.jpeg';
+import BunniesAndEggs from 'images/bunnies_and_eggs.jpeg';
 import Cars from 'images/cars.jpg';
+import Casino from 'images/casino.jpeg';
+import CoffeeAndDonuts from 'images/coffee_and_donuts.jpeg';
 import ColorfulFlowers from 'images/colorful_flowers.jpg';
 import Country from 'images/country.jpg';
 import Crowns from 'images/crowns.jpg';
 import Dancing from 'images/dancing.jpg';
+import DiamondHearts1 from 'images/diamond_hearts_1.jpeg';
+import DiamondHearts2 from 'images/diamond_hearts_2.jpeg';
+import DiamondRings from 'images/diamond_rings.jpeg';
 import DressThankYou from 'images/dress_thank_you.jpg';
 import FallLeaves from 'images/fall_leaves.jpg';
+import FarmAnimals from 'images/farm_animals.jpeg';
 import Fiesta from 'images/fiesta.jpg';
 import FirstBirthday from 'images/first_birthday.jpg';
 import Fishing from 'images/fishing.jpg';
 import Forest from 'images/forest.jpg';
+import FreakinAwesome from 'images/freakin_awesome.jpeg';
 import Graduation from 'images/graduation.jpg';
 import Grills from 'images/grills.jpg';
 import Halloween from 'images/halloween.jpg';
+import HomeDoorsKeys from 'images/home_doors_keys.jpeg';
 import HomeSweetHome from 'images/home_sweet_home.jpg';
+import Irish from 'images/irish.jpeg';
 import Jerseys from 'images/jerseys.jpg';
 import Logo from 'images/logo.jpg';
 import Martinis from 'images/martinis.jpg';
+import MastersGolf from 'images/masters_golf.jpeg';
 import MermaidsAndSeahorses from 'images/mermaids_and_seahorses.jpg';
 import Mermaids from 'images/mermaids.jpg';
 import Nebraska from 'images/nebraska.jpg';
+import SesameStreet from 'images/sesame_street.jpeg';
+import Sports from 'images/sports.jpeg';
 import MultiFlowers from 'images/multi_flowers.jpg';
 import SpringFlowers from 'images/spring_flowers.jpg';
 import Tacos from 'images/tacos.jpg';
 import TeaCups from 'images/tea_cups.jpg';
 import TeaParty from 'images/tea_party.jpg';
+import ThankYouHearts from 'images/thank_you_hearts.jpeg';
 import Titleist from 'images/titleist.jpg';
+import Tonka from 'images/tonka.jpeg';
+import Unicorns from 'images/unicorns.jpeg';
 import Valentines from 'images/valentines_cookies.jpg';
+import WeddingRings from 'images/wedding_rings.jpeg';
 import WeddingShower from 'images/wedding_shower.jpg';
+import WeddingToast from 'images/wedding_toast.jpeg';
+import WhiteHearts from 'images/white_hearts.jpeg';
 
 export default [
+  {
+    src: BabyBoy,
+    thumbnail: BabyBoy,
+    thumbnailWidth: 375,
+    thumbnailHeight: 400,
+  },
   {
     src: BabyShower,
     thumbnail: BabyShower,
     thumbnailWidth: 600,
+    thumbnailHeight: 400,
+  },
+  {
+    src: Baby,
+    thumbnail: Baby,
+    thumbnailWidth: 375,
     thumbnailHeight: 400,
   },
   {
@@ -61,6 +97,12 @@ export default [
     thumbnailHeight: 400,
   },
   {
+    src: BeachTime,
+    thumbnail: BeachTime,
+    thumbnailWidth: 375,
+    thumbnailHeight: 400,
+  },
+  {
     src: BirdsAndFlowers,
     thumbnail: BirdsAndFlowers,
     thumbnailWidth: 600,
@@ -79,9 +121,39 @@ export default [
     thumbnailHeight: 400,
   },
   {
+    src: Bridesmaids,
+    thumbnail: Bridesmaids,
+    thumbnailWidth: 350,
+    thumbnailHeight: 400,
+  },
+  {
+    src: BrownBears,
+    thumbnail: BrownBears,
+    thumbnailWidth: 325,
+    thumbnailHeight: 400,
+  },
+  {
+    src: BunniesAndEggs,
+    thumbnail: BunniesAndEggs,
+    thumbnailWidth: 325,
+    thumbnailHeight: 400,
+  },
+  {
     src: Cars,
     thumbnail: Cars,
     thumbnailWidth: 600,
+    thumbnailHeight: 400,
+  },
+  {
+    src: Casino,
+    thumbnail: Casino,
+    thumbnailWidth: 375,
+    thumbnailHeight: 400,
+  },
+  {
+    src: CoffeeAndDonuts,
+    thumbnail: CoffeeAndDonuts,
+    thumbnailWidth: 375,
     thumbnailHeight: 400,
   },
   {
@@ -109,6 +181,24 @@ export default [
     thumbnailHeight: 400,
   },
   {
+    src: DiamondHearts1,
+    thumbnail: DiamondHearts1,
+    thumbnailWidth: 325,
+    thumbnailHeight: 400,
+  },
+  {
+    src: DiamondHearts2,
+    thumbnail: DiamondHearts2,
+    thumbnailWidth: 400,
+    thumbnailHeight: 400,
+  },
+  {
+    src: DiamondRings,
+    thumbnail: DiamondRings,
+    thumbnailWidth: 400,
+    thumbnailHeight: 400,
+  },
+  {
     src: DressThankYou,
     thumbnail: DressThankYou,
     thumbnailWidth: 300,
@@ -118,6 +208,12 @@ export default [
     src: FallLeaves,
     thumbnail: FallLeaves,
     thumbnailWidth: 500,
+    thumbnailHeight: 400,
+  },
+  {
+    src: FarmAnimals,
+    thumbnail: FarmAnimals,
+    thumbnailWidth: 375,
     thumbnailHeight: 400,
   },
   {
@@ -145,6 +241,12 @@ export default [
     thumbnailHeight: 400,
   },
   {
+    src: FreakinAwesome,
+    thumbnail: FreakinAwesome,
+    thumbnailWidth: 400,
+    thumbnailHeight: 400,
+  },
+  {
     src: Graduation,
     thumbnail: Graduation,
     thumbnailWidth: 600,
@@ -163,9 +265,21 @@ export default [
     thumbnailHeight: 400,
   },
   {
+    src: HomeDoorsKeys,
+    thumbnail: HomeDoorsKeys,
+    thumbnailWidth: 375,
+    thumbnailHeight: 400,
+  },
+  {
     src: HomeSweetHome,
     thumbnail: HomeSweetHome,
     thumbnailWidth: 400,
+    thumbnailHeight: 400,
+  },
+  {
+    src: Irish,
+    thumbnail: Irish,
+    thumbnailWidth: 375,
     thumbnailHeight: 400,
   },
   {
@@ -175,9 +289,21 @@ export default [
     thumbnailHeight: 400,
   },
   {
+    src: Logo,
+    thumbnail: Logo,
+    thumbnailWidth: 300,
+    thumbnailHeight: 400,
+  },
+  {
     src: Martinis,
     thumbnail: Martinis,
     thumbnailWidth: 600,
+    thumbnailHeight: 400,
+  },
+  {
+    src: MastersGolf,
+    thumbnail: MastersGolf,
+    thumbnailWidth: 375,
     thumbnailHeight: 400,
   },
   {
@@ -190,6 +316,24 @@ export default [
     src: Mermaids,
     thumbnail: Mermaids,
     thumbnailWidth: 600,
+    thumbnailHeight: 400,
+  },
+  {
+    src: Nebraska,
+    thumbnail: Nebraska,
+    thumbnailWidth: 500,
+    thumbnailHeight: 400,
+  },
+  {
+    src: SesameStreet,
+    thumbnail: SesameStreet,
+    thumbnailWidth: 375,
+    thumbnailHeight: 400,
+  },
+  {
+    src: Sports,
+    thumbnail: Sports,
+    thumbnailWidth: 375,
     thumbnailHeight: 400,
   },
   {
@@ -223,9 +367,39 @@ export default [
     thumbnailHeight: 400,
   },
   {
+    src: ThankYouHearts,
+    thumbnail: ThankYouHearts,
+    thumbnailWidth: 375,
+    thumbnailHeight: 400,
+  },
+  {
+    src: Titleist,
+    thumbnail: Titleist,
+    thumbnailWidth: 800,
+    thumbnailHeight: 400,
+  },
+  {
+    src: Tonka,
+    thumbnail: Tonka,
+    thumbnailWidth: 375,
+    thumbnailHeight: 400,
+  },
+  {
+    src: Unicorns,
+    thumbnail: Unicorns,
+    thumbnailWidth: 375,
+    thumbnailHeight: 400,
+  },
+  {
     src: Valentines,
     thumbnail: Valentines,
     thumbnailWidth: 700,
+    thumbnailHeight: 400,
+  },
+  {
+    src: WeddingRings,
+    thumbnail: WeddingRings,
+    thumbnailWidth: 375,
     thumbnailHeight: 400,
   },
   {
@@ -235,21 +409,15 @@ export default [
     thumbnailHeight: 400,
   },
   {
-    src: Logo,
-    thumbnail: Logo,
-    thumbnailWidth: 300,
+    src: WeddingToast,
+    thumbnail: WeddingToast,
+    thumbnailWidth: 400,
     thumbnailHeight: 400,
   },
   {
-    src: Nebraska,
-    thumbnail: Nebraska,
-    thumbnailWidth: 500,
-    thumbnailHeight: 400,
-  },
-  {
-    src: Titleist,
-    thumbnail: Titleist,
-    thumbnailWidth: 800,
+    src: WhiteHearts,
+    thumbnail: WhiteHearts,
+    thumbnailWidth: 375,
     thumbnailHeight: 400,
   },
 ];
